81410df4061fb35ffbdaf402f4b3b8bbc70717bb
Rémi Capillon <rcapillon@yahoo.fr>
2025-07-09 17:24:39 +0200

    added Material class

--- src/eigenfem/materials.cpp
+++ src/eigenfem/materials.cpp
@@ -2,3 +2,32 @@
 // materials.cpp
 //
 
+#include "materials.h"
+
+
+ Eigen::MatrixXf Material::compute_mat_C()
+{
+    float Y = Material::young;
+    float nu = Material::poisson;
+    float lame1 = (Y * nu) / ((1 + nu) * (1 - 2 * nu));
+    float lame2 = Y / (2 * (1 + nu));
+
+    Eigen::MatrixXf mat_C(6, 6);
+    mat_C(0, 0) = lame1;
+    mat_C(0, 1) = lame1;
+    mat_C(0, 2) = lame1;
+    mat_C(1, 0) = lame1;
+    mat_C(1, 1) = lame1;
+    mat_C(1, 2) = lame1;
+    mat_C(2, 0) = lame1;
+    mat_C(2, 1) = lame1;
+    mat_C(2, 2) = lame1;
+    mat_C(0, 0) += 2 * lame2;
+    mat_C(1, 1) += 2 * lame2;
+    mat_C(2, 2) += 2 * lame2;
+    mat_C(3, 3) = 2 * lame2;
+    mat_C(4, 4) = 2 * lame2;
+    mat_C(5, 5) = 2 * lame2;
+
+    return mat_C;
+}
--- src/eigenfem/materials.h
+++ src/eigenfem/materials.h
@@ -2,3 +2,28 @@
 // materials.h
 //
 
+#ifndef materials_h
+#define materials_h
+
+#include "../../third-party/eigen-3.4.0/Eigen/Core"
+
+
+class Material {
+    public:
+        Material(float rho, float Y, float nu) {
+            Material::rho = rho;
+            Material::young = Y;
+            Material::poisson = nu;
+            Material::mat_C = compute_mat_C();
+        };
+        ~Material() {};
+
+        float rho;
+        float young;
+        float poisson;
+        Eigen::MatrixXf mat_C;
+
+        Eigen::MatrixXf compute_mat_C();
+};
+
+#endif
