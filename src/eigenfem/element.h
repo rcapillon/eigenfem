05a496a58c3036494646e3bb8e745bf87e0a38b1
Rémi Capillon <rcapillon@yahoo.fr>
2025-07-10 17:03:15 +0200

    progress on Element class

--- src/eigenfem/element.h
+++ src/eigenfem/element.h
@@ -10,6 +10,7 @@
 #include "../../third-party/eigen-3.4.0/Eigen/Core"
 
 #include "utils.h"
+#include "materials.h"
 
 
 Eigen::MatrixXf mat_G;
@@ -20,5 +21,15 @@ Eigen::MatrixXf nodes_reference_coords;
 Eigen::MatrixXf shapefun_coeffs;
 
 MatsGauss mats_gauss;
+
+class Element
+{
+    public:
+        Element(int number, Material material, std::vector<int> nodes_num, Eigen::Vector3f nodes_coords)
+        {
+        };
+        ~Element() {};
+
+};
 
 #endif
