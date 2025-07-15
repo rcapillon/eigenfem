674f9058562956ece9675c5f2975805795c8d1ab
Rémi Capillon <rcapillon@yahoo.fr>
2025-07-15 17:49:42 +0200

    removed unused dependency

--- third-party/MshIO-0.0.1/python/mshio/__init__.py
+++
@@ -1,3 +0,0 @@
-""" A tiny C++ library to read/write ASCII/binary MSH format files.
-"""
-from .pymshio import *
--- third-party/MshIO-0.0.1/src/load_msh_post_process.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <mshio/MshSpec.h>
-
-namespace mshio {
-
-void load_msh_post_process(MshSpec& spec);
-
-}
