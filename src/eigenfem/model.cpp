7c12583501ea2edc8d948916874d46a4da5da7f0
Rémi Capillon <rcapillon@yahoo.fr>
2025-07-16 08:52:55 +0200

    progress on Model class

--- src/eigenfem/model.cpp
+++ src/eigenfem/model.cpp
@@ -2,3 +2,19 @@
 // model.cpp
 //
 
+#include "model.h"
+
+
+Model::Model(Mesh msh,
+    std::vector<int> dir_tags, 
+    std::vector<std::tuple<int, Eigen::VectorXf>> surf_forces,
+    std::vector<std::tuple<int, Eigen::VectorXf>> vol_forces,
+    float a_M, float a_K) : alpha_M{0.}, alpha_K{0.}
+{
+    mesh = msh;
+    dirichlet_tags = dir_tags;
+    surface_forces = surf_forces;
+    volume_forces = vol_forces;
+    alpha_M = a_M;
+    alpha_K = a_K;
+};
--- src/eigenfem/model.h
+++ src/eigenfem/model.h
@@ -2,3 +2,34 @@
 // model.h
 //
 
+#ifndef model_h
+#define model_h
+
+#include <tuple>
+
+#include "../../third-party/eigen-3.4.0/Eigen/Core"
+#include "../../third-party/eigen-3.4.0/Eigen/Sparse"
+
+#include "mesh.h"
+
+
+class Model
+{
+    public:
+        Model() {};
+        Model(Mesh msh, 
+            std::vector<int> dir_tags, 
+            std::vector<std::tuple<int, Eigen::VectorXf>> surf_forces,
+            std::vector<std::tuple<int, Eigen::VectorXf>> vol_forces,
+            float a_M, float a_K) {};
+        ~Model() {};
+
+        Mesh mesh;
+        std::vector<int> dirichlet_tags;
+        std::vector<std::tuple<int, Eigen::VectorXf>> surface_forces;
+        std::vector<std::tuple<int, Eigen::VectorXf>> volume_forces;
+        float alpha_M;
+        float alpha_K;
+};
+
+#endif
