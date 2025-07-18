e4cc165fe75e3c6d69d224efc41c64114344bdb2
Rémi Capillon <rcapillon@yahoo.fr>
2025-07-18 15:07:26 +0200

    added eigensolver

--- src/eigenfem/solvers.cpp
+++ src/eigenfem/solvers.cpp
@@ -2,3 +2,57 @@
 // solvers.cpp
 //
 
+#include <iostream>
+#include <utility>
+#include <cmath>
+
+#include "solvers.h"
+
+
+ModalSolver::ModalSolver(Model mdl)
+{
+    model = mdl;
+}
+
+void ModalSolver::solve(int n_modes)
+{
+    model.create_dof_lists();
+    model.assemble_M_K();
+    model.apply_dirichlet();
+
+    Spectra::SparseSymMatProd<float> Kop(model.mat_Kff);
+    Spectra::SparseCholesky<float>  Mop(model.mat_Mff);
+
+    int ncv = 2 * n_modes;
+    Spectra::SymGEigsSolver<
+        Spectra::SparseSymMatProd<float>, 
+        Spectra::SparseCholesky<float>, 
+        Spectra::GEigsMode::Cholesky> geigs(Kop, Mop, n_modes, ncv);
+    geigs.init();
+    int nconv = geigs.compute(Spectra::SortRule::SmallestMagn);
+
+    if (geigs.info() == Spectra::CompInfo::Successful)
+    {
+        Eigen::VectorXf eigenvalues = geigs.eigenvalues();
+        mat_modes = geigs.eigenvectors();
+
+        std::vector<float> vec_eigenvalues(eigenvalues.data(), eigenvalues.data() + eigenvalues.size());
+        std::vector<std::pair<float, int>> vec_eigvals_to_sort;
+        for (int i = 0; i < vec_eigenvalues.size(); i++)
+        {
+            vec_eigvals_to_sort.push_back(std::make_pair(vec_eigenvalues[i], i));
+        }
+        std::sort(vec_eigvals_to_sort.begin(), vec_eigvals_to_sort.end(), pair_comparator);
+        std::vector<int> sort_indices;
+        for (size_t i = 0; i < vec_eigvals_to_sort.size(); i++)
+        {
+            vec_freqs.push_back(sqrt(vec_eigvals_to_sort[i].first));
+            sort_indices.push_back(vec_eigvals_to_sort[i].second);
+        }
+        mat_modes = mat_modes(Eigen::all, sort_indices);
+    }
+    else
+    {
+        std::cout << "Eigensolver did not converge." << std::endl;
+    }
+}
--- src/eigenfem/solvers.h
+++ src/eigenfem/solvers.h
@@ -2,3 +2,31 @@
 // solvers.h
 //
 
+#ifndef solvers_h
+#define solvers_h
+
+#include "../../third-party/eigen-3.4.0/Eigen/Core"
+#include "../../third-party/spectra-1.1.0/include/Spectra/SymGEigsSolver.h"
+#include "../../third-party/spectra-1.1.0/include/Spectra/MatOp/SparseSymMatProd.h"
+#include "../../third-party/spectra-1.1.0/include/Spectra/MatOp/SparseCholesky.h"
+
+#include "model.h"
+#include "utils.h"
+
+
+class ModalSolver
+{
+    public:
+        ModalSolver() {};
+        ModalSolver(Model mdl);
+        ~ModalSolver() {};
+
+        Model model;
+
+        std::vector<float> vec_freqs;
+        Eigen::MatrixXf mat_modes;
+
+        void solve(int n_modes);
+};
+
+#endif
