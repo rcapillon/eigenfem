fc3f155487f62bcf82d7d1916226a110fdd2aa47
Rémi Capillon <rcapillon@yahoo.fr>
2025-07-19 14:42:46 +0200

    started work on outputing VTK files

--- src/eigenfem/io.cpp
+++ src/eigenfem/io.cpp
@@ -1,3 +1,20 @@
 //
 // io.cpp
 //
+
+#include "io.h"
+
+
+VTKwriter::VTKwriter(Mesh msh, Eigen::VectorXf vec_displacement)
+{
+    mesh = msh;
+    U = vec_displacement;
+}
+
+void VTKwriter::write_deformed_mesh(std::string path_to_file)
+{
+    // std::ofstream myfile;
+    // myfile.open("example.txt");
+    // myfile << "Writing this to a file.\n";
+    // myfile.close();
+}
--- src/eigenfem/io.h
+++ src/eigenfem/io.h
@@ -1,3 +1,29 @@
 //
 // io.h
 //
+
+#ifndef io_h
+#define io_h
+
+#include <fstream>
+#include <string>
+
+#include "../../third-party/eigen-3.4.0/Eigen/Core"
+
+#include "mesh.h"
+
+
+class VTKwriter
+{
+    public:
+        VTKwriter() {};
+        VTKwriter(Mesh msh, Eigen::VectorXf vec_displacement);
+        ~VTKwriter() {};
+
+        Mesh mesh;
+        Eigen::VectorXf U;
+
+        void write_deformed_mesh(std::string path_to_file);
+};
+
+#endif
