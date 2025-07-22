b9ddf86691689f5301357665be5145370a8b6198
Rémi Capillon <rcapillon@yahoo.fr>
2025-07-22 23:54:58 +0200

    current main is a frequency sweep on fine mesh (rom is loaded)

--- src/eigenfem/main.cpp
+++ src/eigenfem/main.cpp
@@ -21,7 +21,7 @@ int main()
     Material steel(7800., 2.1e11, 0.3); 
     Material aluminium(2700., 7e10, 0.33);
 
-    std::string mesh_path = "../data/plate.mesh";
+    std::string mesh_path = "../data/plate_fine.mesh";
     Material material = aluminium;
     Mesh mesh(mesh_path, material); // Chosen material will be used for the whole domain
     mesh.import_gmsh_matlab(); // Constructs coordinates and connectivity tables
@@ -32,15 +32,15 @@ int main()
 
     // Defines the surface force vector
     Eigen::VectorXf vec_surface_force = Eigen::VectorXf::Zero(3);
-    vec_surface_force(0) = 2e9;
+    vec_surface_force(0) = 1.25e10;
 
     std::tuple<int, Eigen::VectorXf> tuple_surface_force = std::make_tuple(surface_force_tag, vec_surface_force);
     std::vector<std::tuple<int, Eigen::VectorXf>> surf_forces;
     surf_forces.push_back(tuple_surface_force);
 
     // Rayleigh damping coefficients
-    float alpha_M = 1e0;
-    float alpha_K = 1e-4;
+    float alpha_M = 1e-6;
+    float alpha_K = 1e-6;
 
     Model model(mesh, dirichlet_tags, surf_forces, {}, alpha_M, alpha_K);
     
@@ -49,8 +49,8 @@ int main()
     FrequencySweepSolver solver(model);
     int n_modes = 10;
     int n_freqs = 300;
-    float min_w = 2 * PI * 1700;
-    float max_w = 2 * PI * 2100;
+    float min_w = 2 * PI * 1000;
+    float max_w = 2 * PI * 2000;
     float delta_w = (max_w - min_w) / (n_freqs - 1);
     float current_w = min_w - delta_w;
     std::vector<float> angular_freqs;
@@ -61,8 +61,10 @@ int main()
     }
     
     time_t rom_timer_start = time(nullptr); // Starts timer for reduced-order model construction
-    std::cout << "Computing ROM..." << std::endl;
-    solver.compute_rom(n_modes);
+    std::cout << "Loading ROM..." << std::endl;
+    DATio dat_io = DATio();
+    Eigen::MatrixXf rom_basis = dat_io.load_dat("../data/plate_fine_rom_basis.dat") ;
+    solver.load_rom(rom_basis);
     time_t rom_timer_end = time(nullptr); // Ends timer for reduced-order model construction
     time_t solver_timer_start = time(nullptr); // Starts timer for solver execution
     std::cout << "Solving for all frequencies..." << std::endl;
